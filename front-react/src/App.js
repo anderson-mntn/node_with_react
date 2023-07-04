96706540c63095bb6c348d9e061e32b7c24e9748
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-07-04 10:27:28 -0300

    add: fetch para url dos videos, thumbnail dos videos via react

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,22 +1,33 @@
+import React, { useEffect, useState } from "react"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=>{
+    fetch('/api/videos').then(res => res.json()).then(data =>{
+      setVideos(data)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+        <div>
+          {videos.map((id, index) => {
+            return (<a key={index} href={`https://youtube.com/watch?v=${id}`}>
+            <img src={`https://img.youtube.com/vi/${id}/0.jpg`} alt={"video alt n." + index}></img>
+            </a>)
+          })}
+        </div>
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { error } = require('console');
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const videosUrls = [
     'KUIsVXKuVAk',
@@ -9,6 +11,15 @@ const videosUrls = [
 
 app.get('/api/videos', (req, res) =>{
     res.send(videosUrls);
+})
+
+app.use(express.static(path.join(__dirname, 'front-react/build')))
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'front-react/build/index.html'), function(error){})
+    if(error){
+        res.status(500).send(error);
+    }
 })
 
 app.listen(3000, () =>{
